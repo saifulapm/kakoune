saifulapm/kakoune

commit d098a64e135884c4a6891ea02b0e655e21c1e024

Fix some posix compliance in man.kak

rc/man.kak
@@ -38,7 +38,7 @@ def -params .. \
     prefix=${1:0:${kak_pos_in_token}}
     for page in /usr/share/man/*/${prefix}*.[1-8]*; do
         candidate=$(basename ${page%%.[1-8]*})
-        pagenum=$(sed -r 's,^.+/.+\.([1-8][^.]*)\..+$,\1,' <<< $page)
+        pagenum=$(echo $page | sed 's/^.*\([1-8][a-z]*\)$/\1/')
         case $candidate in
             *\*) ;;
             *) echo $candidate\($pagenum\);;

commit 24513aeb226e90d7be06662346c796278e94432a

Fix a regression introduced in d098a64e.
The regular expression changed, and returned the wrong page number to
the command completion.

rc/man.kak
@@ -38,7 +38,7 @@ def -params .. \
     prefix=${1:0:${kak_pos_in_token}}
     for page in /usr/share/man/*/${prefix}*.[1-8]*; do
         candidate=$(basename ${page%%.[1-8]*})
-        pagenum=$(echo $page | sed 's/^.*\([1-8][a-z]*\)$/\1/')
+        pagenum=$(echo $page | sed -r 's,^.+/.+\.([1-8][^.]*)\..+$,\1,')
         case $candidate in
             *\*) ;;
             *) echo $candidate\($pagenum\);;