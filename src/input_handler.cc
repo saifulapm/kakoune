saifulapm/kakoune

commit e9d063bd9be71d384db6153bd47ad9bd52cc1b1a

Fix crash when deleting a buffer from a user mapping
Deleting a buffer resets normal mode on all clients that were
displaing that buffer, but ScopedForceNormalMode that are used
from user mode  do not take this possiblity into account on
destruction, which leads to deleting the last normal mode from
the context, ending up with an empty mode stack.

Fixes #3909

src/input_handler.cc
@@ -1698,14 +1698,9 @@ InputHandler::ScopedForceNormal::~ScopedForceNormal()
 
     if (m_mode == m_handler.m_mode_stack.back().get())
         m_handler.pop_mode(m_mode);
-    else
-    {
-        auto it = find_if(m_handler.m_mode_stack,
-                          [this](const RefPtr<InputMode>& m)
-                          { return m.get() == m_mode; });
-        kak_assert(it != m_handler.m_mode_stack.end());
+    else if (auto it = find(m_handler.m_mode_stack, m_mode);
+             it != m_handler.m_mode_stack.end())
         m_handler.m_mode_stack.erase(it);
-    }
 }
 
 static bool is_valid(Key key)

src/ref_ptr.hh
@@ -88,8 +88,8 @@ struct RefPtr
         acquire();
     }
 
-    friend bool operator==(const RefPtr& lhs, const RefPtr& rhs) { return lhs.m_ptr == rhs.m_ptr; }
-    friend bool operator!=(const RefPtr& lhs, const RefPtr& rhs) { return lhs.m_ptr != rhs.m_ptr; }
+    friend bool operator==(const RefPtr& lhs, const RefPtr& rhs) = default;
+    friend bool operator==(const RefPtr& lhs, const T* rhs) { return lhs.m_ptr == rhs; }
 
 private:
     T* m_ptr = nullptr;

test/regression/3909-crash-on-closing-buffer-with-user-mapping/cmd
@@ -1,0 +1,1 @@
+i<a-;>,d<esc>

test/regression/3909-crash-on-closing-buffer-with-user-mapping/in
@@ -1,0 +1,1 @@
+

test/regression/3909-crash-on-closing-buffer-with-user-mapping/out
@@ -1,0 +1,1 @@
+

test/regression/3909-crash-on-closing-buffer-with-user-mapping/rc
@@ -1,0 +1,2 @@
+edit -scratch
+map global user d :delete-buffer<ret>

commit b553fab92688ac6fb4e17ad8b1442fe98da1412b

Remove invalid assert in ScopedForceNormal destructor
commit e9d063bd9be71d384db6153bd47ad9bd52cc1b1a introduced logic
to handle the case where the mode had been removed, but did not
get rid of the assert.

src/input_handler.cc
@@ -1694,8 +1694,6 @@ InputHandler::ScopedForceNormal::~ScopedForceNormal()
     if (not m_mode)
         return;
 
-    kak_assert(m_handler.m_mode_stack.size() > 1);
-
     if (m_mode == m_handler.m_mode_stack.back().get())
         m_handler.pop_mode(m_mode);
     else if (auto it = find(m_handler.m_mode_stack, m_mode);