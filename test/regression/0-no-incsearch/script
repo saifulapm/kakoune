saifulapm/kakoune

commit 07b69988f9bd3c4d2e5fe69995b78a01405ad7c3

Fix partial regex text being pushed in history

src/normal.cc
@@ -853,23 +853,22 @@ void regex_prompt(Context& context, String prompt, char reg, T func)
                         context.window().set_position(position);
 
                     context.input_handler().set_prompt_face(context.faces()["Prompt"]);
+                    RegisterManager::instance()[reg].restore(context, saved_reg);
                 }
+
                 switch (event)
                 {
-                    case PromptEvent::Change:
-                        if (not incsearch or str.empty())
-                            return;
+                case PromptEvent::Abort: return;
+                case PromptEvent::Change:
+                    if (incsearch and not str.empty())
                         RegisterManager::instance()[reg].set(context, str.str());
-                        func(Regex{str, direction_flags(mode)}, event, context);
-                        return;
-                    case PromptEvent::Abort:
-                        RegisterManager::instance()[reg].restore(context, saved_reg);
-                        return;
-                    case PromptEvent::Validate:
-                        context.push_jump();
-                        func(Regex{str.empty() ? default_regex : str, direction_flags(mode)}, event, context);
-                        return;
+                    break;
+                case PromptEvent::Validate:
+                    RegisterManager::instance()[reg].set(context, str.str());
+                    context.push_jump();
+                    break;
                 }
+                func(Regex{str.empty() ? default_regex : str, direction_flags(mode)}, event, context);
             }
             catch (regex_error& err)
             {

test/regression/0-intermediate-regex-saved-in-history/cmd
@@ -1,0 +1,1 @@
+

test/regression/0-intermediate-regex-saved-in-history/in
@@ -1,0 +1,1 @@
+foo bar

test/regression/0-intermediate-regex-saved-in-history/kak_selection
@@ -1,0 +1,1 @@
+foo

test/regression/0-intermediate-regex-saved-in-history/script
@@ -1,0 +1,5 @@
+ui_out -ignore 4
+ui_in '{ "jsonrpc": "2.0", "method": "keys", "params": [ "/foo<ret>/b" ] }'
+sleep 0.1
+ui_in '{ "jsonrpc": "2.0", "method": "keys", "params": [ "ar<ret>/<up><up><ret>" ] }'
+ui_out -ignore 1

commit 9b97417fbc8d446e79e4b9eaeaa35e4d2c213685

Fix spurious incremental search when incsearch=false
Regressed in 07b69988f (Fix partial regex text being pushed in history,
2023-11-02).

src/normal.cc
@@ -860,7 +860,9 @@ void regex_prompt(Context& context, String prompt, char reg, T func)
                 {
                 case PromptEvent::Abort: return;
                 case PromptEvent::Change:
-                    if (incsearch and not str.empty())
+                    if (not incsearch)
+                        return;
+                    if (not str.empty())
                         RegisterManager::instance()[reg].set(context, str.str());
                     break;
                 case PromptEvent::Validate:

test/regression/0-no-incsearch/cmd
@@ -1,0 +1,1 @@
+

test/regression/0-no-incsearch/in
@@ -1,0 +1,3 @@
+
+x1
+x2

test/regression/0-no-incsearch/kak_selection
@@ -1,0 +1,1 @@
+x1

test/regression/0-no-incsearch/rc
@@ -1,0 +1,1 @@
+set-option global incsearch false

test/regression/0-no-incsearch/script
@@ -1,0 +1,6 @@
+ui_out -ignore 4
+ui_in '{ "jsonrpc": "2.0", "method": "keys", "params": [ "/x" ] }'
+sleep 0.1
+ui_in '{ "jsonrpc": "2.0", "method": "keys", "params": [ ".<ret>" ] }'
+sleep 0.1
+ui_out -ignore 1